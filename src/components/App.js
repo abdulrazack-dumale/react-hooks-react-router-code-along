dcfbf9b5eb0a1bdb28ac4b18adbf0d767b2a95bc
abdulrazack-dumale <musharfabdul@gmail.com>
2022-09-29 17:31:24 +0300

    finished assignment

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home"
+import NavBar from "./NavBar"
+
+
+function App() {
+  return (    
+    <>
+      <NavBar />
+
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </>
+  );
+}
+export default App;
